427fba49c10a8646e22867860b76430f3ce47a13
matveisut <sutormin.p@gmail.com>
2025-06-25 20:10:31 +0300

    1

--- kb.py
+++ kb.py
@@ -1,1 +1,12 @@
-    
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+
+menu = [
+    [InlineKeyboardButton(text= 'Гэмини', callback_data = 'generate_text')],
+    InlineKeyboardButton(text= 'Генерировать изображение', callback_data = 'generate_image'),
+    InlineKeyboardButton(text= 'Генерировать звук', callback_data = 'generate_sound')
+]
+
+menu = InlineKeyboardMarkup(inline_keyboard=menu)
+
+exit_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="◀️ Выйти в меню")]], resize_keyboard=True)
+iexit_kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="◀️ Выйти в меню", callback_data="menu")]])
