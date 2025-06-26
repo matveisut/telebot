03242d4137d35bd6014521b2e72ad0e276095ee9
matveisut <sutormin.p@gmail.com>
2025-06-26 11:26:14 +0300

    4

---
+++ utils.py
@@ -0,0 +1,38 @@
+from google import genai
+from google.genai import types
+import config
+import logging
+
+
+def get_gemini_client():
+    """Инициализирует и возвращает клиент Gemini API."""
+    global _gemini_client
+    if _gemini_client is None:
+        _gemini_client = genai.Client(api_key=config.gemini_api)
+    return _gemini_client
+
+
+def return_text(query, system_prompt='ты человек'):
+    
+    
+    try:
+
+
+        client = genai.Client(api_key=config.gemini_api) 
+
+        response = client.models.generate_content(
+                model="gemini-2.5-flash",
+                contents=query,
+                config=types.GenerateContentConfig(
+                    thinking_config=types.ThinkingConfig(thinking_budget=0),
+                    system_instruction=system_prompt
+                ),
+            )
+        
+        return response.text
+    except Exception as e:
+        logging.error(f"Ошибка в return_text_async: {e}")
+        return None # Или поднять исключение, в зависимости от логики
+
+    
+print(return_text('расскажи шутку'))
